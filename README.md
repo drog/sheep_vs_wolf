907759a4ab2cd621c304accda49a24af4b6ac81d
Diego Canales <dcanales2010@alu.uct.cl>
2013-06-21 14:56:49 -0400

    fix directories

---
+++ README.md
@@ -0,0 +1,4 @@
+sheep_vs_wolf
+=============
+
+Game in sfml 2 and tmx for map tiles
---
+++ src/config.hpp
@@ -0,0 +1,14 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 480;
+const int SPRITE_SIZE = 32;
+const int SPRITE_SIZE_TERRAIN = 10;
+const int MOV = 8;
+const int DEPTH = 16;
+const int NUM_SHEEP =10;
+const int NUM_WOLF = 6;
+
+
+#endif 
